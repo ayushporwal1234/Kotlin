a9d4a7ff419ca678cb1e0c54e63bbaaa6c5164f6
Ayush Porwal <78747905+ayushporwal1234@users.noreply.github.com>
2024-07-06 12:35:21 +0530

    Learning Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -14,4 +14,14 @@ fun main() {
     // false ! --> true
     val answer = false
     println(!answer)
+
+    // short-circuiting
+
+    var i = 10
+    var j = 11
+    var result = i == 10 || j++ == 11
+    //Logic:- here or operator check the first condition which is "i" and then directly it will produce answer without check second condition
+    println(i)
+    println(j)
+
 }
---
+++ src/whenStatement/LearnWhenStatement.kt
@@ -0,0 +1,25 @@
+package whenStatement
+
+fun main()
+{
+//    val number = 5
+//    //val result = number in 1..5  // upper bound included --> true
+//    val result = number in 1 until 5 // upper bound excluded --> false
+//        println(result)
+
+            val number  = 15
+          //when as expression
+           val result = when(number) {
+              11 -> "Eleven"
+              12 -> "Twelve"
+               in 13..19 -> "Teen"
+               else -> " Human error"
+           }
+//            when(animal){
+//                "Horse" -> println("It's Horse")
+//                "Dog" -> println("It's Dog")
+//                "Cat" -> println("It's Cat")
+//               else -> println("It's Human")
+//            }
+            println(result)
+}
