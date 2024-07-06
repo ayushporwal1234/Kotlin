7e4fd0a975f7c5dfb29b2fcfb9144bf59aa86e16
Ayush Porwal <78747905+ayushporwal1234@users.noreply.github.com>
2024-07-06 11:40:03 +0530

    Learning Kotlin

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    //Logical Operator
+
+    //||(or)
+    //!(Not)
+
+        val above70 =  false;
+        val knowsProgram = false;
+          //&&(And :- both condition are true then it will true)
+        val calledForInterview = above70 && knowsProgram
+        println(calledForInterview)
+    }
